44c62fb7aa4bf34de34ca1522247a859869c351d
impressivecoder1 <rohanrahman251@gmail.com>
2026-03-03 01:49:03 +0600

    class three

--- src/Pages/Book/Book.jsx
+++ src/Pages/Book/Book.jsx
@@ -1,24 +1,25 @@
+import { Star } from 'lucide-react';
 import React from 'react';
 
 
 const Book = ({singleBook}) => {
     console.log(singleBook)
     return (
-        <div className="card bg-base-100 w-96 shadow-sm">
-  <figure className='p-4 '>
-    <img className=''
+        <div className="card bg-base-100 w-96 shadow-sm border p-6">
+  <figure className='p-4  bg-gray-200 w-2/3 mx-auto'>
+    <img className='h-41.25'
       src={singleBook.image}
       alt="Shoes" />
   </figure>
   <div className="card-body">
     <h2 className="card-title">
-      Card Title
+      {singleBook.bookName}
       <div className="badge badge-secondary">NEW</div>
     </h2>
     <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
     <div className="card-actions justify-end">
-      <div className="badge badge-outline">Fashion</div>
-      <div className="badge badge-outline">Products</div>
+      <div className="badge">{singleBook.category}</div>
+      <div className="badge">{singleBook.rating}<Star/></div>
     </div>
   </div>
 </div>
--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useState } from 'react';
+import React, { Suspense } from 'react';
 import Book from '../Book/Book';
 
 const Books = ({data}) => {
@@ -13,7 +13,7 @@ const Books = ({data}) => {
         <div>
             <h1 className='text-3xl text-center p-6'>Books page</h1>
             <Suspense fallback= {`Loading...`}>
-                <div>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                     {
                     data.map(singleBook => <Book key={singleBook.bookId} singleBook={singleBook}></Book>)
                    }
