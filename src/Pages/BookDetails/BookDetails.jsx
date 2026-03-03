2eb084803fafc261ee3f48f7458a0eb6afe0e28c
impressivecoder1 <rohanrahman251@gmail.com>
2026-03-04 03:08:27 +0600

    added daynamic routes

--- src/Pages/BookDetails/BookDetails.jsx
+++ src/Pages/BookDetails/BookDetails.jsx
@@ -1,9 +1,18 @@
+import { Star } from 'lucide-react';
 import React from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const BookDetails = () => {
+    const {id} = useParams()
+    const bookId = parseInt(id)
+    const data = useLoaderData()
+    const singleBook  = data.find(book=> book.bookId === bookId)
+    console.log(singleBook)
+    const {publisher,review,image,author,bookName} = singleBook
     return (
         <div>
-            <h1>book details</h1>
+            <h1>{publisher}</h1>
+            <img src={image} alt="" />
         </div>
     );
 };
