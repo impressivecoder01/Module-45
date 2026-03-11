d92fffcd503bbae3188b55aa9bf182e4b80c2632
impressivecoder1 <rohanrahman251@gmail.com>
2026-03-12 03:11:03 +0600

    class nine

--- src/Pages/ReadList/ReadList.jsx
+++ src/Pages/ReadList/ReadList.jsx
@@ -9,6 +9,18 @@ const ReadList = () => {
     const data = useLoaderData()
     // console.log(data)
     const [readList, setReadList] = useState([])
+    const [sort, setSort] = useState("")
+    const handleSort = (sortType) => {
+      setSort(sortType)
+      if(sortType == 'pages'){
+        const sortedByPage = [...readList].sort((a,b)=> a.totalPages - b.totalPages)
+        setReadList(sortedByPage)
+      }
+      if(sortType == 'ratings'){
+        const sortByRatings = [...readList].sort((a,b)=> a.rating - b.rating)
+        setReadList(sortByRatings)
+      }
+    }
     useEffect(()=> {
         const storedBookData  = getStoredBook()
         const convertedStoredBooks = storedBookData.map(id => parseInt(id))
@@ -17,6 +29,13 @@ const ReadList = () => {
     },[])
     return (
         <div>
+          <details className="dropdown">
+  <summary className="btn m-1">Sort by: {sort? sort : ''}</summary>
+  <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+    <li><a onClick={()=> handleSort("pages")} >Pages</a></li>
+    <li><a onClick={()=> handleSort("ratings")}>Ratings</a></li>
+  </ul>
+</details>
              <Tabs>
     <TabList>
       <Tab>Read Book List</Tab>
