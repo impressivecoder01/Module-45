a82dac0eb939bd98dc2f4adabba0a9ce23e882ca
impressivecoder1 <rohanrahman251@gmail.com>
2026-03-12 03:15:36 +0600

    sweet aleart added

--- src/Pages/BookDetails/BookDetails.jsx
+++ src/Pages/BookDetails/BookDetails.jsx
@@ -2,7 +2,10 @@ import { Star } from 'lucide-react';
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
 import { addToStoreDB } from '../../utility/addtoDB';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
+const MySwal = withReactContent(Swal)
 const BookDetails = () => {
     const {id} = useParams()
     const bookId = parseInt(id)
@@ -16,6 +19,15 @@ const BookDetails = () => {
       // if data is already exist then show a alert
       // if book not exist then push in the collection or array
       addToStoreDB(id)
+      MySwal.fire({
+  title: <p>Hello World</p>,
+  didOpen: () => {
+    // `MySwal` is a subclass of `Swal` with all the same instance & static methods
+    MySwal.showLoading()
+  },
+}).then(() => {
+  return MySwal.fire(<p>Shorthand works too</p>)
+})
     }
     const {publisher,review,image,author,bookName} = singleBook
     return (
